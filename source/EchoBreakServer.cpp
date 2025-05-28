d3a5c46cd2f7cfb91afef6f19309a1c113a2a445
Niikle <qwerty>
2025-05-28 22:06:17 +0300

    a little refactoring in EchoBreakServer

--- source/EchoBreakServer.cpp
+++ source/EchoBreakServer.cpp
@@ -7,15 +7,16 @@
 #define PORT 6969
 #define REC_PORT 6868
 #define BUFFER_SIZE 256
+#define IP "172.17.213.255"
 
 void receive(int sock, sockaddr_in addr){
     std::cout << "\nstart listening\n";
     char buffer[BUFFER_SIZE];
     memset(buffer, 0, sizeof(buffer));
-    socklen_t addrLen = sizeof(addr);
-    while(1){
-        ssize_t bytesReceived = recvfrom(sock, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr, &addrLen);
-        if(bytesReceived > 0){
+    socklen_t addr_len = sizeof(addr);
+    while(true){
+        ssize_t bytes_received = recvfrom(sock, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr, &addr_len);
+        if(bytes_received > 0){
             std::cout << buffer << std::endl;
             std::cout << "received some data\n";
         }
@@ -26,7 +27,7 @@ void receive(int sock, sockaddr_in addr){
 int main() {
     int sock;
     int opt = 1;
-    struct sockaddr_in broadcastAddr;
+    struct sockaddr_in broadcast_addr;
 
     // sending
     if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -40,35 +41,35 @@ int main() {
         return -1;
     }
 
-    memset(&broadcastAddr, 0, sizeof(broadcastAddr));
-    broadcastAddr.sin_family = AF_INET;
-    broadcastAddr.sin_port = htons(PORT);
-    broadcastAddr.sin_addr.s_addr = inet_addr("172.17.213.255"); 
+    memset(&broadcast_addr, 0, sizeof(broadcast_addr));
+    broadcast_addr.sin_family = AF_INET;
+    broadcast_addr.sin_port = htons(PORT);
+    broadcast_addr.sin_addr.s_addr = inet_addr(IP); 
 
     //receiving
     int rec_sock;
-    struct sockaddr_in broadcastAddr_rec;
+    struct sockaddr_in broadcast_addr_rec;
     if ((rec_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         std::cout << "receving socket error";
         close(rec_sock);
         return -1;
     }
 
-    memset(&broadcastAddr_rec, 0, sizeof(broadcastAddr_rec));
-    broadcastAddr_rec.sin_family = AF_INET;
-    broadcastAddr_rec.sin_addr.s_addr = INADDR_ANY;
-    broadcastAddr_rec.sin_port = htons(REC_PORT);
+    memset(&broadcast_addr_rec, 0, sizeof(broadcast_addr_rec));
+    broadcast_addr_rec.sin_family = AF_INET;
+    broadcast_addr_rec.sin_addr.s_addr = INADDR_ANY;
+    broadcast_addr_rec.sin_port = htons(REC_PORT);
 
-    if (bind(rec_sock, (struct sockaddr*)&broadcastAddr_rec, sizeof(broadcastAddr_rec)) < 0) {
+    if (bind(rec_sock, (struct sockaddr*)&broadcast_addr_rec, sizeof(broadcast_addr_rec)) < 0) {
         std::cout << "receiving binding err";
         close(rec_sock);
         return -1;
     }
     std::string message;
     std::cout << "Enter the message in format(hostname;cmd/func;command) to exit write exit: ";
-    socklen_t addrLen = sizeof(broadcastAddr_rec);
+    socklen_t addr_len = sizeof(broadcast_addr_rec);
 
-    std::thread rec(receive, rec_sock, broadcastAddr_rec);
+    std::thread rec(receive, rec_sock, broadcast_addr_rec);
 
     while(true){
         //sending
@@ -76,7 +77,7 @@ int main() {
         if(message == "exit"){
             break;
         }
-        sendto(sock, message.c_str(), message.size(), 0, (struct sockaddr*)&broadcastAddr, sizeof(broadcastAddr));
+        sendto(sock, message.c_str(), message.size(), 0, (struct sockaddr*)&broadcast_addr, sizeof(broadcast_addr));
         message = "";
     }
     rec.join();
